b9079a322056f594cef30d7bae5a61695c1671e2
aruZeta <me@aruzeta.com>
2023-03-12 12:34:13 +0100

    [add] Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,13 +66,14 @@ android {
 }
 
 dependencies {
-    val composeVersion = "1.2.0"
-    val hiltVersion = "2.43.2"
+    val composeVersion = "1.3.3"
+    val lifecycleVersion = "2.6.0"
+    val hiltVersion = rootProject.extra["hiltVersion"]
     val retrofit2Version = "2.9.0"
     val coroutinesVersion = "1.1.0"
 
     // Core
-    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.core:core-ktx:1.9.0")
 
     // System UI
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.27.0")
@@ -81,11 +82,11 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     // Lifecycle
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
 
     // Compose
-    implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("androidx.activity:activity-compose:1.6.1")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     implementation("androidx.compose.material3:material3:1.1.0-alpha08")
@@ -106,7 +107,7 @@ dependencies {
     implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
 
     // Retrofit Gson
-    implementation("com.squareup.retrofit2:converter-gson:2.5.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
